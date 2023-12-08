0ab6801b261a54893be1ff05fcbbf29256c24ce5
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2023-12-08 16:19:16 -0300

    Resolução de bug

--- app/src/main/java/br/com/dispositivosmoveis/gestorfinanceiropessoal/Models/Conta.kt
+++ app/src/main/java/br/com/dispositivosmoveis/gestorfinanceiropessoal/Models/Conta.kt
@@ -1,8 +1,8 @@
 package br.com.dispositivosmoveis.gestorfinanceiropessoal.Models
 
 data class Conta(
-    val nome: String = null,
-    val data: String = null,
-    val valor: Float = null,
-    val descricao: String = null
+    val nome: String? = null,
+    val data: String? = null,
+    val valor: Float? = null,
+    val descricao: String? = null
 )
